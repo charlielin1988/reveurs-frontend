e15f79c600257069aef8162b50fba5223e519080
shelbylin13 <shelbylin13@gmail.com>
2022-02-09 13:07:57 -0600

    added .env file

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,23 +1,31 @@
 <template>
-<div>Home</div>
+<div>Home
+  <div class='locations'>
+    <LocationCard />
+  </div>
+</div>
+
 </template>
 <script>
 import axios from 'axios'
+import LocationCard from '../components/LocationCard.vue'
+// const API_URL = process.env.VUE_APP_BASE_URL
+
 export default {
   name: 'Home',
-  components: {},
+  components: {LocationCard},
   data: () => ({
     locations: [],
     searchQuery: '',
     searchResults: [],
     searched: false
   }),
-  mounted:async function(){
+  mounted: async function(){
     await this.getLocations()
   },
   methods: {
     async getLocations() {
-      const res = await axios.get(`http://localhost:8000/locations`)
+      const res = await axios.get(`http:localhost:8000/locations`)
       this.locations = res
       console.log(res)
     }
