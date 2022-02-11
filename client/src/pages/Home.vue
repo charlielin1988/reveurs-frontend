cde55e30502e4c2d23024577cc7b4ee77f1407e4
shelbylin13 <shelbylin13@gmail.com>
2022-02-11 15:10:28 -0600

    added review card

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -3,14 +3,17 @@
   <div class="homepage-header">
     <img src ='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffc00.deviantart.net%2Ffs71%2Ff%2F2014%2F138%2Fd%2Fe%2Fcdrheader_by_melloskitten-d7itirs.png' alt="cirqe des reves"/>
   </div>
+  <a href ="http://localhost:8080/locations">
+  <div class= "homepage-image">
+  <img src = 'https://i.pinimg.com/originals/26/f7/0b/26f70baef901de175d738a67f9a9ff86.png' alt="night circus tent" />
+  </div>
+  </a>
 </div>
 </template>
 
 <style>
 body {
   height: 100vh;
-  background-image:url(
-'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimg05.deviantart.net%2F1b2c%2Fi%2F2013%2F202%2F2%2Fa%2Fle_cirque_des_reves__close_up__by_xxsirinxx-d6ejnty.jpg');
-background-size: cover;
+  background-color:gray
 }
 </style>
